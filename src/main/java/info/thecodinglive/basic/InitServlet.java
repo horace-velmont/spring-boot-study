6cb5290b4b4622a75339b151f0c3c5a46503f2da
Horace Velmont <dragond7@naver.com>
2019-12-29 23:39:33 +0900

    2장까지 학습

---
+++ src/main/java/info/thecodinglive/basic/InitServlet.java
@@ -0,0 +1,29 @@
+package info.thecodinglive.basic;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "initServlet", urlPatterns = {"/init"},
+		initParams = {@WebInitParam(name = "siteName", value = "jpub")})
+public class InitServlet extends HttpServlet {
+	private String myParam = "";
+	
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		System.out.println("init call by velmont");
+		this.myParam = servletConfig.getInitParameter("siteName");
+		System.out.println("입력 사이트명 : " + myParam);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().println("hello");
+	}
+}
---
+++ src/main/java/info/thecodinglive/session/DefaultSessionServlet.java
@@ -0,0 +1,24 @@
+package info.thecodinglive.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/session")
+public class DefaultSessionServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.println("<html><head><title>Session</title></head><body>");
+		HttpSession session = req.getSession();
+		out.println("sessionId::" + session.getId() +"<br/>");
+	}
+}
